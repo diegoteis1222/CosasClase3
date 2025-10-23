e78f1aa4f71a29ea766b207b00985d5fb2eece1a
diegoteis1222 <diegoteis122@gmail.com>
2025-10-23 12:11:29 +0200

    quitado workspace_js

--- Interfaces/workspace_javascript/portfolio/.astro/content-modules.mjs
+++
@@ -1,4 +0,0 @@
-
-export default new Map([
-["src/content/posts/segundo.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fsegundo.mdx&astroContentModuleFlag=true")]]);
-		
